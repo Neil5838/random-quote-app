260ed625bcc941ac29ea1fd121c2c56f2499c89b
Neil5838 <neilSE5838@gmail.com>
2025-01-19 21:19:45 +0545

    fixed bug

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const quoteTitle = document.getElementById("quote");
 const authorName = document.getElementById("author");
 const generateBtn = document.getElementById("generate-btn");
 
+quoteTitle.innerHTML = quotes[0].text;
+authorName.innerHTML = quotes[0].author;
+
 generateBtn.addEventListener('click', function() {
     const randomIndex = Math.floor(Math.random() * quotes.length);
     const {text, author} = quotes[randomIndex];
-    quoteTitle.textContent = text;
-    authorName.textContent = author;
+    quoteTitle.innerHTML = text;
+    authorName.innerHTML = author;
 });
